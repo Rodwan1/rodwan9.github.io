497f02f309366dbebe6fca261659e80812a8b264
Rodwan1 <145416047+Rodwan1@users.noreply.github.com>
2023-11-22 21:55:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
@@ -16,19 +17,18 @@ randomize.addEventListener('click', result);
 
 function result() {
 
+  if(customName.value !== '') {
+    const name = customName.value;
+    let newStory = storyText.replace('Bob', name);
+  }
+
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300 * 0.0714286) + ' stone';
     const temperature =  Math.round((94 - 32) * 5/9) + ' centigrade';
-    newStory = newStory.replace( temperature);
-    newStory = newStory.replace( weight);
-
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
   let newStory = storyText;
 
   let xItem = randomValueFromArray(insertX);
@@ -42,4 +42,3 @@ function result() {
   story.textContent = newStory;
   story.style.visibility = 'visible';
 }
-
